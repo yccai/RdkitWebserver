5058e8ae3478a2adc5a86e3134fe907d0007a16b
Ubuntu <ubuntu@ip-172-31-8-207.eu-west-1.compute.internal>
2016-03-08 21:51:20 +0000

    SVG support added

--- smiles/urls.py
+++ smiles/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/validate/
     url(r'^([^J][A-Za-z0-9@+\-\[\]\(\)\\=#$]+)/validate$', views.validate, name='validate'),
     url(r'^([^J][A-Za-z0-9@+\-\[\]\(\)\\=#$]+)/png$', views.png, name='png'),
+    url(r'^([^J][A-Za-z0-9@+\-\[\]\(\)\\=#$]+)/svg$', views.svg, name='svg'),
 ]
--- smiles/views.py
+++ smiles/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 import rdkit
 from rdkit import Chem
 from rdkit.Chem import Draw
+from rdkit.Chem import rdDepictor
+from rdkit.Chem.Draw import rdMolDraw2D
+
 import uuid
 
 def index(request):
@@ -34,3 +37,27 @@ def png(request, smiles):
 	response = HttpResponse(mimetype="image/png")
 	image.save(response, "PNG")
 	return response
+
+def moltosvg(mol,molSize=(450,150),kekulize=True):
+    mc = Chem.Mol(mol.ToBinary())
+    if kekulize:
+        try:
+            Chem.Kekulize(mc)
+        except:
+            mc = Chem.Mol(mol.ToBinary())
+    if not mc.GetNumConformers():
+        rdDepictor.Compute2DCoords(mc)
+    drawer = rdMolDraw2D.MolDraw2DSVG(molSize[0],molSize[1])
+    drawer.DrawMolecule(mc)
+    drawer.FinishDrawing()
+    svg = drawer.GetDrawingText()
+    # It seems that the svg renderer used doesn't quite hit the spec.
+    # Here are some fixes to make it work in the notebook, although I think
+    # the underlying issue needs to be resolved at the generation step
+    return svg #.replace('svg:','')
+
+def svg(request, smiles):
+        mol = Chem.MolFromSmiles(str(smiles))
+	svg = moltosvg(mol)
+        return HttpResponse(svg, mimetype="image/svg+xml")
+
